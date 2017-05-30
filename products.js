917924119d76a72b5cb2fd20f037aa23f10a5364
Arwa <arwa.it15@gmail.com>
2017-05-30 10:30:33 -0500

    created createProduct function

---
+++ products.js
@@ -0,0 +1,11 @@
+var function createProduct(productName, productDescription, productPrice, productColor, imgURL)
+{
+	var product = {};
+
+	product.productName = productName;
+	product.productDescription =productDescription;
+	product.productPrice = productPrice;
+	product.productColor = productColor;
+	product.imgURL = imgURL;
+}
+
