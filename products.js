f3dee704444f39714b794d09a8b80ec05373f3f5
Chandler Morrison <cwm795@gmail.com>
2017-05-30 12:07:25 -0500

    removed example line

--- products.js
+++ products.js
@@ -13,8 +13,6 @@ function createProduct(productName, productDescription, productPrice, productCol
 }
 
 
-createProduct('ball pen', '1mm', 3, 'balck', 'image/cartierDiamond');
-
 var parentUl = document.getElementById("productList");
 var nodeToAdd = document.createElement("li");
 var text = document.createTextNode('test');
