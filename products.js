cf29ea451b5f6fea1e27dcde9958ec1698493705
Arwa <arwa.it15@gmail.com>
2017-05-30 13:23:01 -0500

    made array of objects anf few changes to js file

--- products.js
+++ products.js
@@ -3,34 +3,35 @@ function createProduct(productName, productDescription, productPrice, productCol
 	var product = {};
 
 	product.productName = productName;
-	product.productDescription =productDescription;
+	product.productDescription = productDescription;
 	product.productPrice = productPrice;
 	product.productColor = productColor;
 	product.imgURL = imgURL;
+	return product;
 }
 
+var listOfProducts = [
+var monteBlanc = createProduct('MonteBlanc Gold Coated Fountain Pen', '18k Gold', 68000, 'Gold, Silver', 'images/MPfountain pen.jpg'),
 
-createProduct('ball pen', '1mm', 3, 'balck', 'image/cartierDiamond');
+var cartierJaguar = createProduct('Cariter Jaguar Ballpoint Pen', 'Limited Edition Pen', 7800, 'Silver, Black', 'images/CartierJaguar.jpg'),
 
-var parentUl = document.getElementById("productList");
-var nodeToAdd = document.createElement("li");
-var text = document.createTextNode('test');
-nodeToAdd.appendChild(text);
-parentUl.appendChild(nodeToAdd);
+var CartierDiamond =  createProduct('Cartier Diamond Gold Fountain Pen', 'Limited Edition Pen', 5600, 'Diamond, Gold,' 'images/CartierDiamond.jpg'),
 
-createProduct('MonteBlanc Gold Coated Fountain Pen', '18k Gold', 68,000, 'Gold, Silver', 'images/MPfountain pen.jpg');
+var montegrappaRoller = createProduct('Montegrappa Rollerball pen', '18k gold trim', 69000, 'Gold', 'images/Montegrappa.jpeg'),
 
-createProduct('Cariter Jaguar Ballpoint Pen', 'Limited Edition Pen', 7,800, 'Silver, Black', 'images/CartierJaguar.jpg');
+var montegrappaDucale = createProduct('Montegrappa Ducale Black Rose Gold roller', 'Rose Gold Pen', 4800, 'Rose Gold', 'images/MGDucale.jpg' ),
 
-createProduct('Cartier Diamond Gold Fountain Pen', 'Limited Edition Pen', 5,600, 'Diamond, Gold,' 'images/CartierDiamond.jpg');
+var gerber = createProduct('Gerber Survival Pen', 'Rugged Survival Pen', 7, 'Gray', 'images/Gerber.jpg'),
 
-createProduct('Montegrappa Rollerball pen', '18k gold trim', 69,000, 'Gold', 'images/Montegrappa.jpeg');
+var bicBall = createProduct('BIC ballpoint pen', 'Clear reliable pen', 0.25, 'clear, blue', 'images/BIC.jpg'),
 
-createProduct('Montegrappa Ducale Black Rose Gold roller', 'Rose Gold Pen', 4,800, 'Rose Gold', 'images/MGDucale.jpg' );
+var bicEconomy = createProduct('BIC Economy pen', 'Plastic cheap pen', 0.25, 'green', 'images/BICEconomy.jpg'),
+];
 
-createProduct('Gerber Survival Pen', 'Rugged Survival Pen', 7, 'Gray', 'images/Gerber.jpg');
 
-createProduct('BIC ballpoint pen', 'Clear reliable pen', .25, 'clear, blue', 'images/BIC.jpg');
 
-createProduct('BIC Economy pen', 'Plastic cheap pen', .25, 'green', 'images/BICEconomy.jpg');
-
+var parentUl = document.getElementById("productList"); //selecting target <ul>
+var nodeToAdd = document.createElement("li"); //create <li> tag
+var text = document.createTextNode("listOfProducts[0]"); //create text to put inside <li>
+nodeToAdd.appendChild(text); //appending text to li
+parentUl.appendChild(nodeToAdd);//appending li to ul
